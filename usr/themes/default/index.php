489eff6e1d82ffc5f5f15832694ca201861233cb
hua <116322279+balala666888@users.noreply.github.com>
2025-05-03 12:16:41 +0800

    Update index.php

--- usr/themes/default/index.php
+++ usr/themes/default/index.php
@@ -26,8 +26,7 @@ $this->need('header.php');
         <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
             <?php postMeta($this); ?>
             <div class="post-content" itemprop="articleBody">
-               <?php $post->excerpt(350, '...'); ?>
-                <?php $this->content(_t('阅读剩余部分')); ?>
+            <?php $this->content('auto'); ?>
             </div>
         </article>
     <?php endwhile; ?>
